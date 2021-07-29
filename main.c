146f67be6155e2db38efecf61ed7bf5d0509041a
Ian D. Brunton <ian.d.brunton@gmail.com>
2021-07-29 18:37:21 -0300

    tokenise function

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*#include "builtins.h"*/
 
@@ -23,6 +24,34 @@ int execute (char** args) {
 #define ISH_TOKEN_BUFFSIZE 64
 #define ISH_TOKEN_DELIM " \t\r\n\a"
 char** tokenise (char* line) {
+	int buffsize = ISH_TOKEN_BUFFSIZE, position = 0;
+	char** tokens = malloc (buffsize * sizeof (char*));
+	char* token;
+
+	if (!tokens) {
+		fprintf (stderr, "ish: allocation error\n");
+		exit (EXIT_FAILURE);
+	}
+
+	token = strtok (line, ISH_TOKEN_DELIM);
+	while (token != NULL) {
+		tokens[position] = token;
+		position++;
+
+		if (position >= buffsize) {
+			buffsize += ISH_TOKEN_BUFFSIZE;
+			tokens = realloc (tokens, buffsize * sizeof (char*));
+			if (!tokens) {
+				fprintf (stderr, "ish: allocation error\n");
+				exit (EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, ISH_TOKEN_DELIM);
+	}
+
+	tokens[position] = NULL;
+	return tokens;
 }
 
 #define ISH_RL_BUFFER_SIZE 1024
